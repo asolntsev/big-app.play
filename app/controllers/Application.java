b45912beb0e57b0e51a468d8ebf0b12f7f7b0c0b
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-01-21 02:05:09 +0300

    web application with many routes and html files.
    Excessive usage of (unfixed) Router.reverse was ugly slow

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,18 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Router;
 
 
 public class Application extends Controller {
+  static {
+    Router.addRoute(0, "GET", "/", "Web.serveContent", null);
+    for (int i = 0; i < 100; i++) {
+      Router.addRoute("GET", "/hello/{action}.*", "Hello.{action}", null);
+      Router.addRoute("GET", "/hello/{action}/?.*", "Hello.{action}", null);
+      Router.addRoute("GET", "/hello/{action}/", "Hello.{action}", null);
+    }
+  }
 
   public static void index() {
     render();
